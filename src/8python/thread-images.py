27ccad9e84ca0c97932e3810fd7f8a6563e17a7d
shoneill <oneill.shane.h@gmail.com>
2015-09-13 17:27:04 -0400

    lainchan works now and better default directory

--- src/8python/thread-images.py
+++ src/8python/thread-images.py
@@ -17,8 +17,15 @@ class Parser4(HTMLParser):
         super().__init__()
 
     def handle_starttag(self, tag, attrs):
-        if 'img' in tag:
-            print(tag, attrs)
+        # Only parse the 'anchor' tag.
+        if tag == "a":
+           # Check the list of defined attributes.
+           for name, value in attrs:
+               # If href is defined, print it.
+               if name == "href":
+                   base = 'https://media.8ch'
+                   if base in value[:len(base)]:
+                       self.links.append(value)
 
 class Parser8(HTMLParser):
     def __init__(self):
@@ -39,11 +46,23 @@ class Parser8(HTMLParser):
 class ParserLain(HTMLParser):
     def __init__(self):
         self.links = []
+        self.base = "https://lainchan.org"
         super().__init__()
 
     def handle_starttag(self, tag, attrs):
-        if 'img' in tag:
-            print(tag, attrs)
+        # Only parse the 'anchor' tag.
+        if tag == "a":
+            if len(attrs) > 1 and attrs[1] == ('target','_blank'):
+                fragment = attrs[0][1]
+                if fragment[:4] != 'http':
+                    self.links.append(self.base+fragment)
+            #for name, value in attrs:
+            #    if name == "target" and value == "_blank":
+            #        try:
+            #            print(attrs)
+            #            self.links.append(value)
+            #        except:
+            #            pass
 
 class ParserGeneric(HTMLParser):
     def __init__(self):
@@ -56,7 +75,10 @@ class ParserGeneric(HTMLParser):
 
 def main():
     thread = ""
-    directory = time.strftime("%H%M%S")
+    directory = c_time = time.strftime("%H%M%S")
+    i = 0
+    while os.path.isdir(c_time):
+        directory = c_time + '_' + i
     info = "image downloader for 4chan, 8chan, and lainchan"
     parser = argparse.ArgumentParser(description=info)
     parser.add_argument('-d', '--directory',
@@ -79,26 +101,26 @@ def parse(thread):
     parsed_uri = urlparse(thread)
     domain = '{uri.scheme}://{uri.netloc}/'.format(uri=parsed_uri)
     if "lainchan" in domain:
+        print("using Lainchan parser")
         parser = ParserLain()
     elif "8ch" in domain:
+        print("using 8chan parser")
         parser = Parser8()
     elif "4chan" in domain:
+        print("using 4chan parser")
         parser = Parser4()
     else:
+        print("Unknown thread. Using generic parser")
         parser = ParserGeneric()
-    try:
-        print(thread)
-        page = proper_request(thread)
-        parser.feed(str(page.decode('utf-8')))
-    except Exception as e:
-        print("An exception has occurred: ", e.value)
+    page = proper_request(thread)
+    parser.feed(str(page))
     return parser.links
 
 def download(links, directory):
     for url in links:
         print("Downloading " + url + " to " + directory)
         # DEBUGGING. UNCOMMENT LATER
-        subprocess.call(["wget","-q","-P",directory,url])
+        # subprocess.call(["wget","-q","-P",directory,url])
         
 def proper_request(url):
     "spoofs agent, as to not be declined by the site"
@@ -108,6 +130,8 @@ def proper_request(url):
     request=urllib.request.Request(url,None,headers)
     response = urllib.request.urlopen(request)
     data = response.read()
+    if data:
+        print("Recieved response")
     return data
 
 if __name__ == '__main__':
