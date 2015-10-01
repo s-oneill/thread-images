88a80e79227fc0ba92c613f454cf3500beeb3d20
s-oneill <oneill.shane.h@gmail.com>
2015-10-01 11:58:18 -0400

    now a python package!

--- setup.py
+++ setup.py
@@ -3,15 +3,19 @@ try:
 except ImportError:
     from distutils.core import setup
 
+import threadimages
+
 config = {
     'description': 'image downloader',
     'author': 'Shane ONeill',
+    'version': threadimages.__version__,
+    'license': threadimages.__license__,
     'url': 'https://github.com/shoneill/thread-images',
     'download_url': 'https://github.com/shoneill/thread-images',
     'author_email': 'oneill.shane.h@gmail.com',
     'version': '1.0',
-    'scripts'=['bin/thread-images'],
-    'packages': ['thread-images'],
+    'scripts':['bin/thread-images'],
+    'packages': ['threadimages'],
     'name': 'thread-images'
 }
 
